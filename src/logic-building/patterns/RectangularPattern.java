d43d5a816a23223b026d46bf65be55c72dddb6b3
MAC <mac124056@gmail.com>
2026-06-13 22:22:35 +0530

    Day 1 | Logic Building |Rectangular Star Pattern.

---
+++ src/logic-building/patterns/RectangularPattern.java
@@ -0,0 +1,25 @@
+// Problem : Pattern 1 - Rectangular Star Pattern .
+// Approach : Iterate through N rows and N columns using nested loops, Printing " * " at each position .
+// Time Complexity : O(n^2)  & Space Complexity : O(1) .
+// Link : https://takeuforward.org/pattern/pattern-1-rectangular-star-pattern
+
+import java.util.Scanner;
+
+public class RectangularPattern{
+    public static void main(String[] args) {
+
+      try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter n: ");
+            int n = sc.nextInt();
+
+            for(int i = 1; i <= n; i++) {
+
+                for(int j = 1; j <= n; j++) {
+                    System.out.print("* ");
+                }
+
+                System.out.println();
+            }
+        }
+    }
+}
