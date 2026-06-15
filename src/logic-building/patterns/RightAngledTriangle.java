61a755fc819715dbe3f0d50e9c557c79f7e75dfa
MAC <mac124056@gmail.com>
2026-06-15 20:56:40 +0530

    Day 2 |Logic Building |Right Angled Triangle Star Pattern.

---
+++ src/logic-building/patterns/RightAngledTriangle.java
@@ -0,0 +1,25 @@
+// Problem : Pattern-2: Right-Angled Triangle Pattern.
+// Approach : In each row, print stars equal to the row number..
+// Time Complexity : O(n^2)  & Space Complexity : O(1) .
+// Link : https://takeuforward.org/pattern/pattern-2-right-angled-triangle-pattern
+
+
+import java.util.Scanner;
+
+public class RightAngledTriangle{
+    public static void main (String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter n: ");
+            int n = sc.nextInt();
+
+            for(int i = 1; i <= n; i++) {
+
+                for(int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                }
+
+                System.out.println();
+            }
+        }
+    }
+}
