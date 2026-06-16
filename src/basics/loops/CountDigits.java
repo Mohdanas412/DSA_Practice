31647e27f277b08d0b89f877affaafd581cd5f42
MAC <mac124056@gmail.com>
2026-06-16 22:30:46 +0530

    Day 3 |Basics - Loops |Count Digits in Numbers.

---
+++ src/basics/loops/CountDigits.java
@@ -0,0 +1,24 @@
+// Problem : Count digits in a number
+// Approach : Keep dividing by 10 until the number becomes 0; count the divisions..
+// Time Complexity : O(n)  & Space Complexity : O(1) .
+// Link : https://takeuforward.org/data-structure/count-digits-in-a-number.
+
+package basics.loops;
+import java.util.Scanner;
+
+public class CountDigits{
+    public static void main(String [] args) {
+        try(Scanner sc = new Scanner(System.in) ) {
+            System.out.println("Enter n:");
+            int n = sc.nextInt();
+
+            int count = 0;
+            while (n > 0) {
+                count ++ ;
+                n = n / 10 ;
+                
+            }
+            System.out.println(count);
+        }
+    }
+ }
