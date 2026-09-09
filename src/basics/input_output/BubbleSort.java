d1eb78a0a230f291998ec9a0c89b9079f0688e1e
MAC <mac124056@gmail.com>
2026-09-09 21:21:31 +0530

    Implementing bubble sort

---
+++ src/basics/input_output/BubbleSort.java
@@ -0,0 +1,45 @@
+package basics.input_output;
+
+import java.util.*;
+
+public class BubbleSort {
+
+    static void sort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n - 1; i > 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
