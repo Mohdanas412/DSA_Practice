97c57895a203459ebae94637791df043bd4d1dbc
MAC <mac124056@gmail.com>
2026-09-15 23:49:20 +0530

    Check number of duplicate element and remove them and print final array list.

---
+++ src/arrays/DuplicateElem.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import java.util.*;
+
+public class DuplicateElem{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        int j = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] != a[j]) {
+                j++;
+                a[j] = a[i];
+            }
+        }
+
+        System.out.println("Unique elements: " + (j + 1));
+
+        for (int i = 0; i <= j; i++)
+            System.out.print(a[i] + " ");
+    }
+}
