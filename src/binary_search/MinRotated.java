0baf059f2977358905764dad7862731bc21285ae
Mohdanas412 <mac124056@gmail.com>
2026-09-26 23:27:00 +0530

    Binary Search: FInd minimum rotated sorted array.

---
+++ src/binary_search/MinRotated.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class MinRotated {
+
+    public static int findMin(int[] arr) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return arr[low];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(findMin(arr));
+
+        sc.close();
+    }
+}
